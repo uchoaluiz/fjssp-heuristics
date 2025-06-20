2ee5cce47e465615822f796f0757861c9ad9d90b
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-20 05:38:20 -0300

    feat: implements some solution builder methods

--- src/fjssp_heurs/execution.py
+++ src/fjssp_heurs/execution.py
@@ -11,18 +11,20 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
     inst = instance.Instance(instance_path)
     yield f"instance {inst._instance_name} succefully loaded | known optimal = {inst.optimal_solution}"
 
-    """
+    # """
     logger.log("printing built instance")
     with logger:
         inst.print(logger=logger, type="array")
         # inst.print(logger=logger, type="all")
         # inst.print(logger=logger, type="sets")
-    """
+    # """
 
     if method == "cbc" or method == "both":
         yield "solving FJSSP with CBC solver"
         with logger:
-            math_model = model.MathModel(instance=inst, output_folder=output_folder_path, logger=logger)
+            math_model = model.MathModel(
+                instance=inst, output_folder=output_folder_path, logger=logger
+            )
             math_model.run(show_sol=True, verbose=0, time_limit=1800)
 
     if method == "SA" or method == "both":
@@ -32,6 +34,6 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
             # sol.print()
 
             builder = solbuilder.SolutionBuilder(logger=logger)
-            builder.build_solution(solution=sol, machines_strategy= "grasp", sequence_strategy = "greedy")
-
-
+            builder.build_solution(
+                solution=sol, machines_strategy="grasp", sequence_strategy="greedy"
+            )

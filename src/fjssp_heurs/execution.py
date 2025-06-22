f7843781e25f80f74b4fd92e3b592fd66779dd67
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-22 20:37:50 -0300

    fix: fixes execution flow to use new constructive heuristic

--- src/fjssp_heurs/execution.py
+++ src/fjssp_heurs/execution.py
@@ -10,17 +10,18 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
     yield "loading instance"
     inst = instance.Instance(instance_path)
     yield f"instance {inst._instance_name} succefully loaded | known optimal = {inst.optimal_solution}"
+    logger.breakline()
 
     instance_output_path = output_folder_path / inst._instance_name
     instance_output_path.mkdir(exist_ok=True)
 
-    # """
+    """
     logger.log("printing built instance")
     with logger:
         inst.print(logger=logger, type="array")
         # inst.print(logger=logger, type="all")
         # inst.print(logger=logger, type="sets")
-    # """
+    """
 
     if method == "cbc" or method == "both":
         yield "solving FJSSP with CBC solver"
@@ -28,7 +29,7 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
             math_model = model.MathModel(
                 instance=inst, output_path=instance_output_path, logger=logger
             )
-            math_model.optimize(verbose=0, time_limit=1800)
+            math_model.optimize(verbose=0, time_limit=5)
             math_model.print(show_gantt=False)
 
     if method == "SA" or method == "both":
@@ -37,11 +38,12 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
             sol = solution.Solution(
                 instance=inst, logger=logger, output_path=instance_output_path
             )
-            # sol.print()
+            sol.print()
+
+            logger.breakline()
 
             builder = solbuilder.SolutionBuilder(logger=logger)
-            builder.build_solution(
-                solution=sol, machines_strategy="grasp", sequence_strategy="greedy"
-            )
+            builder._define_grasp_alpha(alpha=0.3)
 
-            sol.print(show_gantt=True)
+            builder.build_solution(solution=sol, machines_strategy="grasp")
+            sol.print(show_gantt=False)

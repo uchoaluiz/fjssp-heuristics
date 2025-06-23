a4e34a65c719cc74cae46e75317058dc5a67bca2
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-22 23:59:45 -0300

    feat: adds SA into excution flow

--- src/fjssp_heurs/execution.py
+++ src/fjssp_heurs/execution.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
-from .processing.metaheuristic import solution, solbuilder
-from .instance import instance
-from .processing import model
+from .processing.metaheuristic.solution import Solution
+from .processing.metaheuristic.solbuilder import SolutionBuilder
+from .instance.instance import Instance
+from .processing.model import MathModel
 from .utils.logger import LOGGER
+from .processing.metaheuristic.metaheuristics import Metaheuristics
 
 
 def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: LOGGER):
     yield "loading instance"
-    inst = instance.Instance(instance_path)
-    yield f"instance {inst._instance_name} succefully loaded | known optimal = {inst.optimal_solution}"
-    logger.breakline()
+    inst = Instance(instance_path)
+    yield f"instance {inst._instance_name} succefully loaded | known optimal = {inst.optimal_solution}\n"
 
     instance_output_path = output_folder_path / inst._instance_name
     instance_output_path.mkdir(exist_ok=True)
@@ -26,7 +27,7 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
     if method == "cbc" or method == "both":
         yield "solving FJSSP with CBC solver"
         with logger:
-            math_model = model.MathModel(
+            math_model = MathModel(
                 instance=inst, output_path=instance_output_path, logger=logger
             )
             math_model.optimize(verbose=0, time_limit=5)
@@ -35,15 +36,28 @@ def run(*, instance_path: Path, output_folder_path: Path, method: str, logger: L
     if method == "SA" or method == "both":
         yield "solving FJSSP with simulated annealing"
         with logger:
-            sol = solution.Solution(
+            sol = Solution(
                 instance=inst, logger=logger, output_path=instance_output_path
             )
             sol.print()
 
             logger.breakline()
 
-            builder = solbuilder.SolutionBuilder(logger=logger)
+            builder = SolutionBuilder(logger=logger)
             builder._define_grasp_alpha(alpha=0.3)
 
             builder.build_solution(solution=sol, machines_strategy="grasp")
+            sol.schedule_solution()
             sol.print(show_gantt=False)
+
+            metaheur = Metaheuristics()
+            metaheur.sa(
+                sol=sol,
+                alpha=0.97,
+                k=2,
+                beta=1.1,
+                low_temperature=2,
+                final_temperature=0.01,
+                max_time=1800,
+            )
+            sol.print()

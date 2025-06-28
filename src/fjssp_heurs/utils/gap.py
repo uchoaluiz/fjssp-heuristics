3b0c03060f3457cb1cf72d52e9715755a0c8b28f
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-28 19:39:31 -0300

    refac: ensures 'evaluate_gap' correctly handling with na parameters

--- src/fjssp_heurs/utils/gap.py
+++ src/fjssp_heurs/utils/gap.py
@@ -1,2 +1,5 @@
 def evaluate_gap(*, ub: float, lb: float) -> float:
+    if not isinstance(ub, float) or not isinstance(lb, float):
+        return "nan"
+
     return round((100 * (ub - lb) / ub), 4)

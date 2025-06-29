59473862ba6117fd91be8eeddf21cc6c5b6d4c41
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-29 08:14:23 -0300

    feat: adds critical path finder method to Solution class

--- src/fjssp_heurs/processing/metaheuristic/solution.py
+++ src/fjssp_heurs/processing/metaheuristic/solution.py
@@ -31,10 +31,53 @@ class Solution:
         self._machine_sequence = [[] for _ in instance.M]
         self._makespan = 0.0
 
-        self._start_times = np.full(len(instance.O), np.nan)
-        self._finish_times = np.full(len(instance.O), np.nan)
-
+        self._start_times = list()
+        self._finish_times = list()
         logger.log("solution structure built")
+
+    def _find_a_critical_path(self) -> list[int]:
+        instance = self._instance
+        start_times = self._start_times
+        finish_times = self._finish_times
+        assign_vect = self._assign_vect
+        machine_sequence = self._machine_sequence
+        makespan = self._makespan
+
+        end_ops = [op for op in instance.O if finish_times.index(makespan) == op]
+        if not end_ops:
+            raise ValueError(
+                "Nenhuma operação encontrada com término igual ao makespan."
+            )
+
+        current_op = np.random.choice(end_ops)
+        critical_path = [current_op]
+
+        while start_times[current_op] > 0:
+            pred_ops = []
+
+            job = instance.job_of_op[current_op]
+            job_ops = instance.S_j[job]
+            idx_in_job = job_ops.index(current_op)
+            if idx_in_job > 0:
+                prev_job_op = job_ops[idx_in_job - 1]
+                if finish_times[prev_job_op] == start_times[current_op]:
+                    pred_ops.append(prev_job_op)
+
+            machine = int(assign_vect[current_op])
+            mach_ops = machine_sequence[machine]
+            idx_in_machine = mach_ops.index(current_op)
+            if idx_in_machine > 0:
+                prev_mach_op = mach_ops[idx_in_machine - 1]
+                if finish_times[prev_mach_op] == start_times[current_op]:
+                    pred_ops.append(prev_mach_op)
+
+            if not pred_ops:
+                break
+
+            current_op = np.random.choice(pred_ops)
+            critical_path.insert(0, current_op)
+
+        return list(map(lambda x: int(x), critical_path))
 
     def print(self, *, show_gantt: bool = True, gantt_name: str) -> None:
         logger = self._logger

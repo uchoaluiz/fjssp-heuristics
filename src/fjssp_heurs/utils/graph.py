ab6328d42b293ae38b9ad278d9b55dd6eef1b180
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-30 22:31:51 -0300

    refac: refactors graph class to improve readability and modularity

--- src/fjssp_heurs/utils/graph.py
+++ src/fjssp_heurs/utils/graph.py
@@ -14,12 +14,11 @@ class DAG:
         self._graph = nx.DiGraph()
         self._positions = dict()
         self._disjunctive_edge_groups = dict()
-        self._edges = list()
         self._edge_weights = dict()
 
-        self.build_instance_graph()
+        self._build_base_graph()
 
-    def build_instance_graph(self) -> None:
+    def _build_base_graph(self) -> None:
         instance = self._instance
 
         for job, ops in instance.S_j.items():
@@ -37,11 +36,8 @@ class DAG:
         self._positions[op] = (pos_x, -pos_y)
 
     def _add_dependency(self, *, from_node: int, to_node: int, weight: float = 0.0):
-        if weight:
-            self._graph.add_edge(from_node, to_node, weight=weight)
-        else:
-            self._graph.add_edge(from_node, to_node)
-        self._edges.append((from_node, to_node))
+        self._graph.add_edge(from_node, to_node, weight=weight)
+        self._edge_weights[(from_node, to_node)] = weight
 
     def _add_artificial_nodes(self):
         instance = self._instance
@@ -66,12 +62,11 @@ class DAG:
         for ops in instance.S_j.values():
             self._graph.add_edge("S", ops[0], weight=0)
             self._graph.add_edge(ops[-1], "V")
-            self._edges.append((ops[-1], "V"))
 
-    def _add_disjunctive_edge(self, machine, from_node, to_node):
-        if machine not in self._disjunctive_edge_groups:
-            self._disjunctive_edge_groups[machine] = []
-        self._disjunctive_edge_groups[machine].append((from_node, to_node))
+    def _add_disjunctive_edge(self, machine: int, from_node: int, to_node: int):
+        self._disjunctive_edge_groups.setdefault(machine, []).append(
+            (from_node, to_node)
+        )
 
     def draw(
         self,
@@ -80,8 +75,7 @@ class DAG:
         title: str,
         no_disjunctives: bool = False,
         arrowstyle: str = "->",
-        show_weights: bool = False,
-    ):
+    ) -> None:
         plt.figure(figsize=(12, 6))
 
         nx.draw_networkx_nodes(
@@ -100,51 +94,35 @@ class DAG:
             arrowsize=10,
         )
 
-        color_map = cm.get_cmap("tab10")
-        machines = list(self._disjunctive_edge_groups.keys())
-        n_colors = len(machines)
-
-        if show_weights and self._edge_weights:
-            edge_labels = {
-                (u, v): f"{self._edge_weights.get((u, v), '')}" for u, v in self._edges
-            }
-            nx.draw_networkx_edge_labels(
-                self._graph,
-                pos=self._positions,
-                edge_labels=edge_labels,
-                font_color="red",
-                font_size=8,
-            )
+        edge_labels = {
+            (u, v): f"{self._edge_weights.get((u, v), '')}"
+            for u, v in self._graph.edges()
+        }
+        nx.draw_networkx_edge_labels(
+            self._graph,
+            pos=self._positions,
+            edge_labels=edge_labels,
+            font_color="red",
+            font_size=8,
+        )
 
         if no_disjunctives:
             plt.title(f"{title} - without disjunctive")
             plt.axis("off")
             plt.tight_layout()
             plt.savefig(output_path / f"{title} - without disjunctive.png")
+            return None
+
+        color_map = cm.get_cmap("tab10")
+        machines = list(self._disjunctive_edge_groups.keys())
+        n_colors = len(machines)
 
         for i, machine in enumerate(machines):
             color = color_map(i / max(n_colors - 1, 1))
             arcs = self._disjunctive_edge_groups[machine]
 
-            """
-            rad = 0.2 + (i % 3) * 0.2
-            nx.draw_networkx_edges(
-                self._graph,
-                pos=self._positions,
-                edgelist=arcs,
-                edge_color=[color],
-                style="dashed",
-                arrows=True,
-                arrowstyle=arrowstyle,
-                arrowsize=30,
-                width=2,
-                connectionstyle=f"arc3,rad={rad}",
-            )
-            """
-
             rad = 0.2 + (i % 3) * 0.2
 
-            # """
             mult = 1
             for edge in arcs:
                 edge_rad = rad * 1.3 * mult
@@ -162,7 +140,6 @@ class DAG:
                     connectionstyle=f"arc3,rad={edge_rad}",
                 )
                 mult *= -1
-            # """
 
         legend_patches = [
             mpatches.FancyArrowPatch(
@@ -213,12 +190,29 @@ class FJSSPGraph:
         self._instance = instance
         self._machines_assignment = machines_assignment
         self._dag = DAG(instance=instance)
-        self.create_disjunctives(same_job_disjunctives=tech_disjunctives)
+
+        self._create_disjunctives_edges(same_job_disjunctives=tech_disjunctives)
         self._set_edge_weights()
+
+    def _create_disjunctives_edges(self, *, same_job_disjunctives: bool = False):
+        machines_assignment = self._machines_assignment
+
+        for machine, ops in enumerate(machines_assignment):
+            edges = []
+
+            for _from, _to in combinations(ops, 2):
+                if same_job_disjunctives or (
+                    self._instance.job_of_op[_from] != self._instance.job_of_op[_to]
+                ):
+                    edges.extend([(_from, _to)])
+
+            if edges:
+                for edge in edges:
+                    self._dag._add_disjunctive_edge(machine, edge[0], edge[1])
 
     def _set_edge_weights(self):
         instance = self._instance
-        edges = self._dag._edges
+        edges = self._dag._graph.edges()
 
         op_machine_map = {
             op: machine
@@ -228,32 +222,14 @@ class FJSSPGraph:
         }
 
         for edge_from, edge_to in edges:
-            self._dag._edge_weights[(edge_from, edge_to)] = instance.p[
-                (edge_from, op_machine_map[edge_from])
-            ]
-            self._dag._graph[edge_from][edge_to]["weight"] = instance.p[
-                (edge_from, op_machine_map[edge_from])
-            ]
-
-    def create_disjunctives(self, *, same_job_disjunctives: bool = False):
-        instance = self._instance
-        machines_assignment = self._machines_assignment
-
-        for machine, ops in enumerate(machines_assignment):
-            edges = list(combinations(ops, 2))
-            for _from, _to in edges:
-                if _from != _to:
-                    if not same_job_disjunctives:
-                        job_from = instance.job_of_op[_from]
-                        job_to = instance.job_of_op[_to]
-                        if job_from != job_to:
-                            self._dag._add_disjunctive_edge(
-                                machine=machine, from_node=_from, to_node=_to
-                            )
-                    else:
-                        self._dag._add_disjunctive_edge(
-                            machine=machine, from_node=_from, to_node=_to
-                        )
+            if edge_from not in ["S", "V"]:
+                if edge_to not in ["S", "V"]:
+                    self._dag._edge_weights[(edge_from, edge_to)] = instance.p[
+                        (edge_from, op_machine_map[edge_from])
+                    ]
+                    self._dag._graph[edge_from][edge_to]["weight"] = instance.p[
+                        (edge_from, op_machine_map[edge_from])
+                    ]
 
     def consolidate_machine_disjunctive(self, *, machine_assignment: list[int]) -> None:
         instance = self._instance
@@ -280,22 +256,14 @@ class FJSSPGraph:
                 )
 
     def _longest_to(self, *, op: int) -> float:
-        pred_nodes = nx.ancestors(self._dag._graph, op) | {op}
-        subG = self._dag._graph.subgraph(pred_nodes)
-        lengths = nx.dag_longest_path_length(
-            subG,
-            weight="weight",
-        )
-        return lengths
+        subgraph = self._dag._graph.subgraph(nx.ancestors(self._dag._graph, op) | {op})
+        return nx.dag_longest_path_length(subgraph, weight="weight")
 
     def _longest_from(self, *, op: int) -> float:
-        succ_nodes = nx.descendants(self._dag._graph, op) | {op}
-        subG = self._dag._graph.subgraph(succ_nodes)
-        lengths = nx.dag_longest_path_length(
-            subG,
-            weight="weight",
+        subgraph = self._dag._graph.subgraph(
+            nx.descendants(self._dag._graph, op) | {op}
         )
-        return lengths
+        return nx.dag_longest_path_length(subgraph, weight="weight")
 
     def draw_dag(
         self,
@@ -303,12 +271,10 @@ class FJSSPGraph:
         title: str,
         show_no_disjunctives: bool = False,
         arrowstyle: str = "->",
-        show_weights: bool = False,
     ):
         self._dag.draw(
             output_path=output_path,
             title=title,
             no_disjunctives=show_no_disjunctives,
             arrowstyle=arrowstyle,
-            show_weights=show_weights,
         )

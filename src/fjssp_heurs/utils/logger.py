ed726858e6833326df60165bf18c5b91846a4733
uchoaluiz <lotaviouchoa@gmail.com>
2025-06-30 22:27:17 -0300

    feat: adds on/off status to logger

--- src/fjssp_heurs/utils/logger.py
+++ src/fjssp_heurs/utils/logger.py
@@ -2,12 +2,17 @@ class LOGGER:
     def __init__(self, indent_str="    "):
         self.level = 0
         self.indent_str = indent_str
+        self.on = 1
 
     def log(self, message):
-        print(f"{self.indent_str * self.level}> {message}")
+        if self.on == 1:
+            print(f"{self.indent_str * self.level}> {message}")
 
     def breakline(self, n: int = 1):
-        print("\n" * (n-1))
+        print("\n" * (n - 1))
+
+    def switch_on_off(self):
+        self.on *= -1
 
     def __enter__(self):
         self.level += 1

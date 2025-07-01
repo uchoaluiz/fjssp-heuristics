803d110dcbaa15bde58277624a2a2f51a528f07f
uchoaluiz <lotaviouchoa@gmail.com>
2025-07-01 07:20:43 -0300

    feat: adds local logging

--- src/fjssp_heurs/utils/logger.py
+++ src/fjssp_heurs/utils/logger.py
@@ -1,12 +1,40 @@
+from pathlib import Path
+import sys
+
+
 class LOGGER:
-    def __init__(self, indent_str="    "):
+    def __init__(
+        self,
+        log_path: Path,
+        indent_str="    ",
+    ):
+        self._log_path = log_path
         self.level = 0
         self.indent_str = indent_str
         self.on = 1
+        self._log_file = None
+
+        try:
+            self._log_file = open(self._log_path, "w", encoding="utf-8")
+        except IOError as e:
+            print(
+                f"Erro ao abrir o arquivo de log '{self._log_path}': {e}",
+                file=sys.stderr,
+            )
+            self.on = -1
 
     def log(self, message):
-        if self.on == 1:
-            print(f"{self.indent_str * self.level}> {message}")
+        if self.on == 1 and self._log_file:
+            msg = f"{self.indent_str * self.level}> {message}"
+
+            print(msg)
+
+            try:
+                self._log_file.write(msg + "\n")
+                self._log_file.flush()
+            except IOError as e:
+                print(f"Erro ao escrever no arquivo de log: {e}", file=sys.stderr)
+                self.on = -1
 
     def breakline(self, n: int = 1):
         print("\n" * (n - 1))

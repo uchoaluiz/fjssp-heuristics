9fe672b38f52c4cc640f29c6bde8a380fd5c51e6
uchoaluiz <lotaviouchoa@gmail.com>
2025-07-06 20:33:58 -0300

    refactor: add output mode control to LOGGER and improve log handling

--- src/fjssp_heurs/utils/logger.py
+++ src/fjssp_heurs/utils/logger.py
@@ -4,19 +4,16 @@ import datetime
 
 
 class LOGGER:
-    def __init__(
-        self,
-        log_path: Path,
-        indent_str="    ",
-    ):
+    def __init__(self, log_path: Path, indent_str="    ", out: str = "both"):
         self._log_path = log_path
         self.level = 0
         self.indent_str = indent_str
-        self.on = 1
         self._log_file = None
+        self._out = out if out in ["terminal", "file", "both"] else None
+        self.on = 1 if out in ["terminal", "both"] else -1
 
         try:
-            self._log_file = open(self._log_path, "a", encoding="utf-8")
+            self._log_file = open(self._log_path, "w", encoding="utf-8")
 
             self._log_file.write("\n" * 5)
             self._log_file.write(f"{datetime.datetime.now()}\n")
@@ -29,20 +26,24 @@ class LOGGER:
             self.on = -1
 
     def log(self, message):
-        if self.on == 1 and self._log_file:
-            msg = f"{self.indent_str * self.level}> {message}"
-
-            print(msg)
-
-            try:
-                self._log_file.write(msg + "\n")
-                self._log_file.flush()
-            except IOError as e:
-                print(f"error on writing log on file: {e}", file=sys.stderr)
-                self.on = -1
+        msg = f"{self.indent_str * self.level}> {message}"
+        if self._out in ["terminal", "both"]:
+            if self.on == 1:
+                print(msg)
+        if self._out in ["file", "both"]:
+            if self._log_file:
+                try:
+                    self._log_file.write(msg + "\n")
+                except IOError as e:
+                    print(f"error on writing log on file: {e}", file=sys.stderr)
+                    self.on = -1
 
     def breakline(self, n: int = 1):
-        print("\n" * (n - 1))
+        if self.on == 1 and self._out in ["both", "terminal"]:
+            print("\n" * (n - 1))
+        if self._out in ["file", "both"]:
+            for _ in range(n):
+                self._log_file.write("\n")
 
     def switch_on_off(self):
         self.on *= -1
